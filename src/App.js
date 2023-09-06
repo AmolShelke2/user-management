9231700c748181ec64b0709ee573205677747f10
Amol <shelkeamol897@gmail.com>
2023-09-06 18:41:46 +0530

    [feat]: create, read, delete functionality added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { UserList } from "./components/UserList";
 
 function App() {
+  const [userLists, setUserLists] = useState(null);
+  const [newUser, setNewUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  const [showNewUserOverlay, setShowNewUserOverlay] = useState(false);
+
+  const toggleShowNewUserOverlay = () => {
+    setShowNewUserOverlay(!showNewUserOverlay);
+  };
+
+  /** Feteching userLists from the API and storing them in the userLists */
+  useEffect(() => {
+    const fetchUserLists = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+
+      setTimeout(() => setUserLists(data), 1000);
+    };
+
+    fetchUserLists();
+  }, []);
+
+  /** New user onChangeHandler */
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  /** Onsubmit handler */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Define the API endpoint URL
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
+
+    // Create an object with the POST request options
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newUser),
+    };
+
+    // Send the POST request
+    const response = await fetch(apiUrl, requestOptions);
+    const newUserP = await response.json();
+
+    setUserLists((userlists) => [...userlists, newUserP]);
+
+    // clear inputs after submission
+    setNewUser({
+      name: "",
+      phone: "",
+      email: "",
+    });
+  };
+
+  /** Passing this functions to the userList component for onClick callback function */
+  const deleteUser = (userID) => {
+    setUserLists((userlists) => userlists.filter((list) => list.id !== userID));
+  };
+
+  const editUser = (userID) => {
+    console.log("editing user");
+  };
+
+  /** UI elements */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="relative w-full h-full bg-gray-100 flex flex-col items-center">
+        {/* Project title */}
+        <h1 className="text-3xl font-semibold text-gray-600 p-4 mb-4">
+          User Management Application
+        </h1>
+
+        <button
+          className="absolute right-8 top-2 bg-blue-400 py-2 px-8
+         text-white shadow-md cursor-pointer rounded-lg text-sm"
+          onClick={toggleShowNewUserOverlay}>
+          Add New User
+        </button>
+
+        {/* New user form inputs and submit button */}
+        <div
+          className={`${
+            showNewUserOverlay ? "flex" : "hidden"
+          } flex-col justify-start items-start 
+          p-4 m-2 fixed top-12 right-8 shadow-md border border-1 rounded-lg`}>
+          <h2 className="text-[20px] font-semibold text-gray-600 mb-2">
+            Add new user
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-2 mb-4">
+              <label className="text-[18px] font-semibold text-gray-600">
+                name
+              </label>
+              <input
+                type="text"
+                name="name"
+                value={newUser.name}
+                placeholder="Add Full Name"
+                onChange={onChangeHandler}
+                required
+                className="w-[300px] p-3 rounded-lg"
+              />
+            </div>
+            <div className="flex flex-col gap-2 mb-4">
+              <label className="text-[18px] font-semibold text-gray-600">
+                Email
+              </label>
+              <input
+                type="text"
+                name="email"
+                value={newUser.email}
+                placeholder="Add email"
+                onChange={onChangeHandler}
+                required
+                className="w-[300px] p-3 rounded-lg"
+              />
+            </div>
+            <div className="flex flex-col gap-2 mb-4">
+              <label className="text-[18px] font-semibold text-gray-600">
+                Phone
+              </label>
+              <input
+                type="number"
+                name="phone"
+                value={newUser.phone}
+                placeholder="Add phone number"
+                onChange={onChangeHandler}
+                required
+                className="w-[300px] p-3 rounded-lg"
+              />
+            </div>
+            <button
+              type="submit"
+              onSubmit={handleSubmit}
+              className="bg-blue-400 py-2 px-8 text-white shadow-md cursor-pointer rounded-lg text-sm">
+              submit
+            </button>
+          </form>
+        </div>
+
+        {/* Conditionaly rendering userLists component and loader */}
+        {userLists ? (
+          <div className="w-[900px] h-[600px] py-4 px-4 rounded-lg shadow-md border border-1 overflow-y-scroll">
+            {userLists.map((user) => {
+              return (
+                <UserList
+                  key={user.id}
+                  user={user}
+                  deleteUser={deleteUser}
+                  editUser={editUser}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <div className="w-full h-full bg-gray-100 flex flex-col items-center justify-center">
+            <img
+              src="/assets/loader.gif"
+              alt="loading"
+              height={50}
+              width={50}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/UserList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const UserList = ({ user, deleteUser, editUser }) => {
+  return (
+    <div
+      key={user.id}
+      className="w-full flex flex-row justify-between items-center border border-1 mb-4 p-4 rounded-lg">
+      <h3 className="text-[15px] w-[100px] mr-4">{user.name}</h3>
+      <p className="text-[13px] w-[150px] mr-4">{user.email}</p>
+      <p className="text-[13px] w-[130px] mr-4">{user.phone.split("x")}</p>
+
+      <button
+        className=" bg-blue-400 py-2 px-8 text-white shadow-md cursor-pointer rounded-lg text-sm"
+        onClick={() => editUser(user.id)}>
+        Edit
+      </button>
+
+      <button
+        className=" bg-blue-400 py-2 px-8 text-white shadow-md cursor-pointer rounded-lg text-sm"
+        onClick={() => deleteUser(user.id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
